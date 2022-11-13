a795774391e86ff716b36765968841c92880850f
licht <lichtewig73@gmail.com>
2022-11-12 22:20:24 -0300

    probando javascr

---
+++ index.js
@@ -0,0 +1,36 @@
+/*
+let numero = parseInt (prompt ('Ingresa un número: ')
+let numeroDos = parseInt (prompt ('Ingresa un número: '))
+
+let resultado = numero + numeroDos
+
+alert ('El resultado es: ' + resultado )
+
+*/
+
+/*
+let a = 100
+let b = 150
+console.log (a+b)
+*/ 
+
+let numero = parseInt (prompt('Ingresa un numero: '))
+let resultado = numero * 2
+
+alert('El resultado es ' + resultado)
+
+/*
+//2do ejercicio rápido - Calcular área de un círculo
+const pi = 3.14
+let r = 5.3
+const total = pi*r**2
+console.log('Total: ' + total)
+*/
+/*
+let persona = prompt ('Ingresa tu nombre = ')
+
+alert ('Bienvenide + persona')
+*/
+
+
+
