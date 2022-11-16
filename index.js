a2e444c4fc6534131f6302080d59438dbb123d0a
licht <lichtewig73@gmail.com>
2022-11-16 14:23:27 -0300

    comienzo javascr

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ alert ('El resultado es: ' + resultado )
 let a = 100
 let b = 150
 console.log (a+b)
-*/ 
+*/
 
 let numero = parseInt (prompt('Ingresa un numero: '))
 let resultado = numero * 2
@@ -27,10 +27,49 @@ const total = pi*r**2
 console.log('Total: ' + total)
 */
 /*
-let persona = prompt ('Ingresa tu nombre = ')
+let persona = parseInt prompt ('Ingresa tu nombre = ')
 
 alert ('Bienvenide + persona')
+
 */
+//ejercicio 
+/*
+let numero = prompt ('Ingresa un numero')
+let i=1
 
-
-
+while (i<= numero) {
+    if (i%5==0){
+        console.log(i)
+    }
+    i++
+}
+*/
+/**
+ * Usando while, crea un programa que pregunte al usuario un número. 
+ * Mostrar los números que son múltiplos de 5 desde 1 hasta el número introducido por el usuario.
+ */
+/*
+ let numero = prompt('Introduce un número')
+ let i = 1
+ while (i <= numero) { // mi condición es desde 1 hasta el nùmero como marca el problema
+   if(i % 5 == 0) {
+     console.log(i)
+   }
+ 
+   i++
+ }
+ 
+ /**
+  * NOTA: el `%` es divisible de 
+  */
+ 
+// -------------------------------------------------------------------------------------------------------
+ 
+ /**
+  * Usando for, crea un programa que imprima en consola los números impares del 1 al 50.
+  
+ 
+  for(let i =1; i <= 50; i++) {
+   if(i % 2 !== 0) {
+       console.log(i)
+*/
